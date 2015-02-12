be368c58d6ff5e20f56c8e5eb8e900fdbca6111e
luy07 <luy007@msn.cn>
2015-02-12 09:58:12 +0800

    Update stock.py

--- models/stock.py
+++ models/stock.py
@@ -1,3 +1,7 @@
 #coding:utf-8
 from sqlalchemy import Column,String,create_engine
 from sqlalchemy.orm import session_maker
+from sqlalchemy.ext.declarative import declarative_base
+
+# 创建对象的基类:
+Base = declarative_base()
